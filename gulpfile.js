6dce5d7e67b670839df516b8246d35a97dcd620e
Nicholas Brown <nb@brownsfromsa.com>
2025-04-22 22:26:20 -0400

    Added JS linter, compressor, and transpiler

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,9 @@ const {src, dest, watch, series} = require(`gulp`),
     htmlValidator = require(`gulp-html`),
     CSSLinter = require(`gulp-stylelint`),
     csso = require(`gulp-csso`),
-    jsLinter = require(`gulp-eslint`);
-
+    jsLinter = require(`gulp-eslint`),
+    jsCompressor = require(`gulp-uglify`),
+    babel = require(`gulp-babel`);
 
 let compressHTML = () => {
     return src(`app/html/*.html`)
@@ -43,8 +44,29 @@ let lintJS = () => {
         .pipe(jsLinter.formatEach(`compact`));
 };
 
+let transpileJSForDev = () => {
+    return src(`app/js/*.js`)
+        .pipe(babel())
+        .on(`error`, (err) => {
+            console.error(`Babel error:`, err);
+        })
+        .pipe(dest(`./temp/js`))
+        .on(`end`, () => {
+            console.log(`Transpilation complete. Files saved to ./temp/js`);
+        });
+};
+
+let transpileJSForProd = () => {
+    return src(`app/js/*.js`)
+        .pipe(babel())
+        .pipe(jsCompressor())
+        .pipe(dest(`prod/js`));
+};
+
 exports.compressHTML = compressHTML;
 exports.validateHTML = validateHTML;
 exports.lintCSS = lintCSS;
 exports.compileCSSForProd = compileCSSForProd;
 exports.lintJS = lintJS;
+exports.transpileJSForDev = transpileJSForDev;
+exports.transpileJSForProd = transpileJSForProd;
